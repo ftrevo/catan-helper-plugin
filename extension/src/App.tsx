be3a12b3f20fe63214c9ce9f9e9acd6b27798293
Felipe Trevisan <fel.trevisan@gmail.com>
2025-06-27 20:15:07 +0200

    fix: best vertex font weight

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -6,7 +6,7 @@ import { TabHeader, type Tab } from './components/TabHeader/TabHeader'
 import { readImageCb } from './infra/repository'
 
 const mockData = {
-  numbers: ['8', '10', '5', '4', '3', '9', '2', '11', '7', '11', '4', '6', '12', '6', '5', '3', '9', '10', '8'],
+  numbers: ['8', '10', '5', '4', '3', '9', '2', '11', '7', '11', '5', '6', '12', '10', '4', '3', '9', '6', '8'],
   resources: [
     'wool',
     'stone',
