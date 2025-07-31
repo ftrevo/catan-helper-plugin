7872b290fa137feee931b78cc4229f702a23b48e
Felipe Trevisan <fel.trevisan@gmail.com>
2025-07-31 20:41:24 +0200

    feat: adding tab size to inbound payload

--- back/src/v1/endpoints/image/read/validation/request.ts
+++ back/src/v1/endpoints/image/read/validation/request.ts
@@ -2,6 +2,12 @@ import { z } from 'zod'
 
 export const readImageRequestSchema = z.object({
   image: z.string().min(1),
+  tabSize: z
+    .object({
+      height: z.number().optional(),
+      width: z.number().optional(),
+    })
+    .optional(),
 })
 
 export type ReadImageRequestParams = z.infer<typeof readImageRequestSchema>
